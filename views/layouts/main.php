97d1343a278fd991df5d911dcc8cb91abe541fe5
Javier Cabanillas <Javier@MacBook-Pro-de-Javier.local>
2016-06-09 20:35:44 -0500

    Se agrega yii2-jui a requiere

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -1,78 +1,79 @@
 <?php
+/* @var $this \yii\web\View */
+/* @var $content string */
 
-    /* @var $this \yii\web\View */
-    /* @var $content string */
+use jcabanillas\inspinia\AppAsset;
+use yii\helpers\Html;
+use yii\bootstrap\Nav;
+use yii\bootstrap\NavBar;
+use yii\widgets\Breadcrumbs;
+use yii\bootstrap\Alert;
 
-    use jcabanillas\inspinia\AppAsset;
-    use yii\helpers\Html;
-    use yii\bootstrap\Nav;
-    use yii\bootstrap\NavBar;
-    use yii\widgets\Breadcrumbs;
-    use common\widgets\Alert;
-
-    AppAsset::register($this);
-    $directoryAsset = Yii::$app->assetManager->getPublishedUrl('@vendor/jcabanillas/yii2-inspinia/assets');
+AppAsset::register($this);
+$directoryAsset = Yii::$app->assetManager->getPublishedUrl('@vendor/jcabanillas/yii2-inspinia/assets');
 ?>
 <?php $this->beginPage() ?>
 <!DOCTYPE html>
 <html lang="<?= Yii::$app->language ?>">
-<head>
+    <head>
 
-    <meta charset="<?= Yii::$app->charset ?>">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta charset="<?= Yii::$app->charset ?>">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <?= Html::csrfMetaTags() ?>
-    <title><?= Html::encode($this->title) ?></title>
-    <?php $this->head() ?>
-</head>
+        <?= Html::csrfMetaTags() ?>
+        <title><?= Html::encode($this->title) ?></title>
+        <?php $this->head() ?>
+    </head>
 
-<body><?php $this->beginBody() ?>
+    <body><?php $this->beginBody() ?>
 
-<div id="wrapper" class="">
+        <div id="wrapper" class="">
 
-    <?=$this->render('sidebar', ['directoryAsset' => $directoryAsset])?>
+            <?= $this->render('sidebar', ['directoryAsset' => $directoryAsset]) ?>
 
-    <div id="page-wrapper" class="gray-bg">
-        <div class="row border-bottom">
-            <?=$this->render('header', ['directoryAsset' => $directoryAsset])?>
-        </div>
-        <div class="row wrapper border-bottom white-bg page-heading">
-            <?php if (isset($this->blocks['content-header'])) { ?>
-                <?= $this->blocks['content-header'] ?>
-            <?php } else { ?>
-                <div class="col-sm-<?=isset($this->blocks['content-header-actions']) ? 6 : 12?>">
-                    <h2><?=$this->title?></h2>
-
-                    <?=Breadcrumbs::widget([
-                        'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],
-                        'activeItemTemplate' => "<li class=\"active\"><strong>{link}</strong></li>\n"
-                    ]) ?>
+            <div id="page-wrapper" class="gray-bg">
+                <div class="row border-bottom">
+                    <?= $this->render('header', ['directoryAsset' => $directoryAsset]) ?>
                 </div>
-                <?php if(isset($this->blocks['content-header-actions'])):?>
-                    <div class="col-sm-6">
-                        <div class="title-action">
-                            <?=$this->blocks['content-header-actions']?>
+                <div class="row wrapper border-bottom white-bg page-heading">
+                    <?php if (isset($this->blocks['content-header'])) { ?>
+                        <?= $this->blocks['content-header'] ?>
+                    <?php } else { ?>
+                        <div class="col-sm-<?= isset($this->blocks['content-header-actions']) ? 6 : 12 ?>">
+                            <h2><?= $this->title ?></h2>
+
+                            <?=
+                            Breadcrumbs::widget([
+                                'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],
+                                'activeItemTemplate' => "<li class=\"active\"><strong>{link}</strong></li>\n"
+                            ])
+                            ?>
+                        </div>
+    <?php if (isset($this->blocks['content-header-actions'])): ?>
+                            <div class="col-sm-6">
+                                <div class="title-action">
+        <?= $this->blocks['content-header-actions'] ?>
+                                </div>
+                            </div>
+                        <?php endif ?>
+<?php } ?>
+
+                </div>
+
+                <div class="wrapper wrapper-content">
+
+<?= Alert::widget() ?>
+
+                    <div class="row">
+                        <div class="col-lg-12">
+<?= $content ?>
                         </div>
                     </div>
-                <?php endif?>
-            <?php } ?>
-
-        </div>
-
-        <div class="wrapper wrapper-content">
-
-            <?= Alert::widget() ?>
-
-            <div class="row">
-                <div class="col-lg-12">
-                    <?=$content?>
                 </div>
+<?= $this->render('footer', ['directoryAsset' => $directoryAsset]) ?>
             </div>
         </div>
-        <?=$this->render('footer', ['directoryAsset' => $directoryAsset])?>
-    </div>
-</div>
 <?php $this->endBody() ?>
-</body>
+    </body>
 </html>
 <?php $this->endPage() ?>
